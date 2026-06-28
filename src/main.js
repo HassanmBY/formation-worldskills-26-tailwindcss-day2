7a88eabaf14152a097e1c281ea2ea0aed3d264ce
Hassan Mirza <hassanmirza@outlook.be>
2026-06-28 21:11:28 +0200

    FEAT: implement esily maintainable darkmode using @theme and changing theme proporties when dark class is applied to html element

--- src/main.js
+++ src/main.js
@@ -1,1 +1,11 @@
 import './style.css'
+
+
+const toggleMode = document.querySelector('#toggle-mode');
+const htmlElement = document.querySelector('html');
+
+toggleMode.addEventListener('click', e =>{
+    e.preventDefault();
+
+    htmlElement.classList.toggle("dark")
+})
